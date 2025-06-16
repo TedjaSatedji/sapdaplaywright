600fa359180cfe7a67ac005a8339be3a439600cb
TedjaSatedji <dewaagung670@gmail.com>
2025-06-16 12:40:51 +0700

    fix .env wiritng

--- telegbot.py
+++ telegbot.py
@@ -126,7 +126,7 @@ def save_to_env(chat_id, creds):
         f.write(f"\nSPADA_USERNAME_{index}={creds['username']}")
         f.write(f"\nSPADA_PASSWORD_{index}={creds['password']}")
         f.write(f"\nTELEGRAM_CHAT_ID_{index}={chat_id}")
-        f.write(f"\nSCHEDULE_FILE_{index}={schedule_path}\n")
+        f.write(f"\nSCHEDULE_FILE_{index}={schedule_path}\n\n")
 
 def delete_credentials(chat_id):
     """Remove user credentials from .env, including the comment and all related lines."""
